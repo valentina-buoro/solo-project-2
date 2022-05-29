84d1ad84d2d85fadc18b107c5dea30c1dd104c77
queen-x-vee <valentinabuoro4@gmail.com>
2022-05-29 21:37:32 +0100

    update 5-29-2022

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,14 +5,15 @@ export default function Card(props){
         <div>
             <section className='section'>
                 <img className='section-image' src={props.item.image} alt='syd'></img>
-                <div className='section-image'>
+                <div className='sect'>
                     <div className='section-span'>
                         <img src='images/location.png' alt='loc'></img>
                         <span> Australia</span>
                         <span> view on google</span>
-                        <h1>{props.item.title}</h1>
+                        
                     </div>
-                    <h4 className='section-date'>{props.item.startDate} {props.item.endDate}</h4>
+                    <h1>{props.item.title}</h1>
+                    <h4 className='section-date'>{props.item.startDate} - {props.item.endDate}</h4>
                     <p className='section-text'>{props.item.description}</p>
                 </div>
             </section>
